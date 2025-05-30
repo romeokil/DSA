713a167abc94e7781923a0dc7e2431406bafd7c9
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-05-30 16:49:25 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (69.73%) - LeetSync

--- 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
+++ 3172-divisible-and-non-divisible-sums-difference/divisible-and-non-divisible-sums-difference.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     int differenceOfSums(int n, int m) {
-        int firstsum=0;
+        int noofelementsdivisible=n/m;
+        int sumofnoofelementsdivisible=(m*(noofelementsdivisible*(noofelementsdivisible+1)))/2;
         int originalsum=(n*(n+1))/2;
-        for(int i=1;i<=n;i++){
-            if(i%m!=0) firstsum+=i;
-        }
-        cout<<"originalsum->"<<originalsum<<endl;
-        return firstsum==0?-originalsum:firstsum-(originalsum-firstsum);
+        // cout<<"originalsum->"<<originalsum<<endl;
+        // cout<<"sumofelementsdivisible->"<<sumofnoofelementsdivisible<<endl;
+        return originalsum-2*sumofnoofelementsdivisible;
     }
 };
