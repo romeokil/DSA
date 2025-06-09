7f9b232dc961bd462ce2a5b3f301ba457df522f3
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-06-09 22:20:59 +0530

    Time: 3 ms (47.04%) | Memory: 14.3 MB (45.22%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.cpp
+++ 386-lexicographical-numbers/lexicographical-numbers.cpp
@@ -1,21 +1,17 @@
 class Solution {
 public:
-    void solve(int no,vector<int>& temp,int n,unordered_set<int>& st){
+    void solve(int no,vector<int>& temp,int n){
         if(no>n) return ;
-        if(st.find(no)==st.end()){
-            temp.push_back(no);
-            st.insert(no);
-        }
+        temp.push_back(no);
         for(int i=0;i<=9;i++){
             int newno=10*no+i;
-            solve(newno,temp,n,st);
+            solve(newno,temp,n);
         }
     }
     vector<int> lexicalOrder(int n) {
         vector<int> temp;
-        unordered_set<int> st;
         for(int i=1;i<=9;i++){
-            solve(i,temp,n,st);
+            solve(i,temp,n);
         }
         return temp;
     }
