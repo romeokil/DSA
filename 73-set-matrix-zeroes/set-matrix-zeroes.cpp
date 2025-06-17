a92ba1f675aaa9ab9d02213af5b47d6f08698fd2
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-06-17 20:16:17 +0530

    Time: 3 ms (11.39%) | Memory: 19 MB (6.56%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -1,20 +1,13 @@
 class Solution {
-public:
-    void display(vector<vector<int>>& visited,int n,int m){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                cout<<"i->"<<i<<"j->"<<j<<" "<<visited[i][j]<<endl;
-            }
-        }
-    }
-    void paintrowandcol(int row,int col,vector<vector<int>>& visited,int n,int m){
-        // pehle row ko rangte hai
-        for(int i=0;i<n;i++){
-            visited[i][col]=0;
-        }
-        // phir col ko rangte hai.
+public: 
+    void putZero(int row,int col,int n,int m,vector<vector<int>>& visited){
+        // wo saare row me 0 kr dete hai.
         for(int j=0;j<m;j++){
             visited[row][j]=0;
+        }
+        // wo saara column me 0 kr dete hai .
+        for(int k=0;k<n;k++){
+            visited[k][col]=0;
         }
     }
     void setZeroes(vector<vector<int>>& matrix) {
@@ -23,15 +16,13 @@ public:
         vector<vector<int>> visited(n,vector<int>(m,-1));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                // jo bhi row and column me zero mile uske baad function call kr diye
                 if(matrix[i][j]==0){
-                    paintrowandcol(i,j,visited,n,m);
+                    // chalo eska saaara row and column 0 se bhr do
+                    putZero(i,j,n,m,visited);
                 }
             }
         }
-        // hmlog saara matrix se visited me daal dete hai non-zero values
-        cout<<"visted array ko mark krne ke baad"<<endl;
-        display(visited,n,m);
+        // ab rest of visited matrix ko bhr dete hai.
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 if(matrix[i][j]!=0 && visited[i][j]!=0){
@@ -39,9 +30,7 @@ public:
                 }
             }
         }
-        // ab wapas jb visited jb hmlog bhr diye toh wapas matrix me daal diye dete hai.
-        cout<<"ab hmlog matrix se visited ki traf daal de rhe hai chizen";
-        display(visited,n,m);
+        // wapas visited waale se matrix me daal dete hai inplcae bola tha.
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
                 matrix[i][j]=visited[i][j];
