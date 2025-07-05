ec0fb3377fa1f3e0379918021ecd077943c2ba70
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-07-05 23:32:06 +0530

    Time: 0 ms (100.00%) | Memory: 13.9 MB (65.75%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -1,17 +1,31 @@
 class Solution {
 public:
     int findLucky(vector<int>& arr) {
-        unordered_map<int,int> um;
-        for(int i=0;i<arr.size();i++){
-            um[arr[i]]++;
-        }
+        // unordered_map<int,int> um;
+        // for(int i=0;i<arr.size();i++){
+        //     um[arr[i]]++;
+        // }
+        // int luckynumber=-1;
+        // // iterate in the map
+        // for(const pair<int,int>& p:um){
+        //     if(p.first==p.second){
+        //         luckynumber=max(luckynumber,p.first);
+        //     }
+        // }
+        // return luckynumber;
+
+        // without using extra space
         int luckynumber=-1;
-        // iterate in the map
-        for(const pair<int,int>& p:um){
-            if(p.first==p.second){
-                luckynumber=max(luckynumber,p.first);
+        int n=arr.size();
+        vector<int> vec(501,0);
+        for(int i=0;i<n;i++){
+            vec[arr[i]]++;
+        }
+        for(int i=1;i<501;i++){
+            if(vec[i]!=0 && i==vec[i]){
+                luckynumber=max(luckynumber,i);
             }
         }
         return luckynumber;
-    }
+    }   
 };
