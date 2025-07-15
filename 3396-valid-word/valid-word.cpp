07c4ce38f3b561f89f3404dae1b4b16518dd6c30
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-07-15 10:56:21 +0530

    Time: 6 ms (2.43%) | Memory: 7.9 MB (87.14%) - LeetSync

--- 3396-valid-word/valid-word.cpp
+++ 3396-valid-word/valid-word.cpp
@@ -16,7 +16,7 @@ public:
     }
     bool isconsonant(char ch){
         ch=tolower(ch);
-        return !(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u') && !(ch>='0' && ch<='9');
+        return (ch>='a' && ch<='z') && !(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u');
     }
     bool isValid(string word) {
         bool vowel=false,consonant=false;
