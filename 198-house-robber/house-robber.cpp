4f818534b42d53acd5fd580ed68543f46388dbd6
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-07-31 11:37:02 +0530

    Time: 3 ms (5.95%) | Memory: 10.6 MB (64.24%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int solve(int idx,vector<int>& nums,vector<int>& dp){
-        if(idx>=nums.size()) return 0;
-
-        if(dp[idx]!=-1) return dp[idx];
-        // ek ghr lete hai ek ghr chorte hai 
-        int take_case=0,skip_case=0;
-        take_case=nums[idx]+solve(idx+2,nums,dp);
-        skip_case=solve(idx+1,nums,dp);
-        return dp[idx]=max(take_case,skip_case);
-    }
     int rob(vector<int>& nums) {
         int n=nums.size();
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        if(n==2) return max(nums[0],nums[1]);
         vector<int> dp(n+1,-1);
-        return solve(0,nums,dp);
+        dp[0]=0,dp[1]=nums[0],dp[2]=max(nums[0],nums[1]);
+        for(int i=3;i<=n;i++){
+            int take_case=nums[i-1]+dp[i-2];
+            int skip_case=dp[i-1];
+            dp[i]=max(take_case,skip_case);
+        }
+        return dp[n];
     }
 };
