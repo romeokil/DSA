84735ac631b59bdda3287ffbc3efc6c187626906
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-08-20 23:47:04 +0530

    Time: 44 ms (9.27%) | Memory: 188.7 MB (11.62%) - LeetSync

--- 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
+++ 2573-remove-nodes-from-linked-list/remove-nodes-from-linked-list.cpp
@@ -10,45 +10,44 @@
  */
 class Solution {
 public:
-    void reverse(ListNode* &newhead,ListNode* curr){
-        // 
-        if(curr->next==NULL){
-            newhead=curr;
-            return ;
+    void reverse(ListNode*& newhead, ListNode* curr) {
+        //
+        if (curr->next == NULL) {
+            newhead = curr;
+            return;
         }
-        reverse(newhead,curr->next);
-        ListNode* prev=curr;
-        curr->next->next=prev;
-        curr->next=NULL;
+        reverse(newhead, curr->next);
+        ListNode* prev = curr;
+        curr->next->next = prev;
+        curr->next = NULL;
     }
     ListNode* removeNodes(ListNode* head) {
         stack<int> st;
-        ListNode* temp=head;
-        while(temp!=NULL){
-            while(!st.empty()  && (temp->val)>st.top()){
+        ListNode* temp = head;
+        while (temp != NULL) {
+            while (!st.empty() && (temp->val) > st.top()) {
                 st.pop();
             }
             st.push(temp->val);
-            temp=temp->next;
+            temp = temp->next;
         }
-        ListNode* newhead=NULL;
-        temp=newhead;
-        while(!st.empty()){
-            int top=st.top();
-            if(newhead==NULL){
-                ListNode* newNode=new ListNode(top);
-                newhead=newNode;
-                temp=newhead;
-            }
-            else{
-                ListNode *newNode=new ListNode(top);
-                temp->next=newNode;
-                temp=temp->next;
+        ListNode* newhead = NULL;
+        temp = newhead;
+        while (!st.empty()) {
+            int top = st.top();
+            if (newhead == NULL) {
+                ListNode* newNode = new ListNode(top);
+                newhead = newNode;
+                temp = newhead;
+            } else {
+                ListNode* newNode = new ListNode(top);
+                temp->next = newNode;
+                temp = temp->next;
             }
             st.pop();
         }
-        ListNode* curr=newhead;
-        reverse(newhead,curr);
+        ListNode* curr = newhead;
+        reverse(newhead, curr);
         return newhead;
     }
 };
