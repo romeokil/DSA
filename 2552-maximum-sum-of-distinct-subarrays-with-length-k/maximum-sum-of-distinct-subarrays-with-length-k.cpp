15bbcb19ebdcecf9e5e886fcbcdaa5c31b68de9a
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-08-21 15:21:16 +0530

    Time: 103 ms (62.05%) | Memory: 95.2 MB (93.58%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -1,30 +1,32 @@
 class Solution {
 public:
-    bool checkdistinct(unordered_map<int,int>& um){
-        for(const pair<int,int>& p:um){
-            if(p.second>1) return false;
+    bool checkdistinct(unordered_map<int, int>& um) {
+        for (const pair<int, int>& p : um) {
+            if (p.second > 1)
+                return false;
         }
         return true;
     }
     long long maximumSubarraySum(vector<int>& nums, int k) {
-        int n=nums.size();
-        int i=0,j=0;
-        long long cursum=0,maxsum=INT_MIN;
-        unordered_map<int,int> um;
-        while(j<n){
-            cursum+=nums[j];
+        int n = nums.size();
+        int i = 0, j = 0;
+        long long cursum = 0, maxsum = INT_MIN;
+        unordered_map<int, int> um;
+        while (j < n) {
+            cursum += nums[j];
             um[nums[j]]++;
-            if(i<j && (j-i+1)>k){
-                cursum-=nums[i];
+            if (i < j && (j - i + 1) > k) {
+                cursum -= nums[i];
                 um[nums[i]]--;
-                if(um[nums[i]]==0) um.erase(nums[i]);
+                if (um[nums[i]] == 0)
+                    um.erase(nums[i]);
                 i++;
             }
-            if((j-i+1)==k && um.size()==k){
-                maxsum=max(maxsum,cursum);
+            if ((j - i + 1) == k && um.size() == k) {
+                maxsum = max(maxsum, cursum);
             }
             j++;
         }
-        return maxsum==INT_MIN?0:maxsum;
+        return maxsum == INT_MIN ? 0 : maxsum;
     }
 };
