acea92ea9a07660a87e8adc9eb0948e88bf42d80
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-08-28 19:46:56 +0530

    Time: 0 ms (100.00%) | Memory: 20.9 MB (24.21%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.cpp
+++ 547-number-of-provinces/number-of-provinces.cpp
@@ -1,23 +1,33 @@
 class Solution {
 public:
-    void dfs(int source,vector<vector<int>>& isConnected,vector<bool>& visited){
+    void dfs(int source,vector<vector<int>>& um,vector<bool>& visited){
         visited[source]=true;
-        // explore its neighbour;
-        for(int j=0;j<isConnected[source].size();j++){
-            // isConnected [i] [j] = 1 ye basically wha pe denote kr rha hai city
-            if(!visited[j] && isConnected[source][j]==1){
-                dfs(j,isConnected,visited);
+        // explore its neighbour
+        for(int &v:um[source]){
+            if(!visited[v]){
+                dfs(v,um,visited);
             }
         }
     }
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n=isConnected.size();
-        vector<bool> visited(n,false);
         int count=0;
+        // ek or ghatiya approach 
+        vector<bool> visited(n,false);
+        // lets make a adjacency graph
+        vector<vector<int>> um(n);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<isConnected[i].size();j++){
+                if(isConnected[i][j]==1){
+                    um[i].push_back(j);
+                    um[j].push_back(i);
+                }
+            }
+        }
+        // ab dekho normal dfs wala code likh skte hai.
         for(int i=0;i<n;i++){
             if(!visited[i]){
-                // jo bhi connected component hai usko mark kr dega.
-                dfs(i,isConnected,visited);
+                dfs(i,um,visited);
                 count++;
             }
         }
