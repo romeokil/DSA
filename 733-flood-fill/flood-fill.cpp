ea3ce1b48754416d6824a2d87f1608e86928acb9
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-08-30 12:23:52 +0530

    Time: 0 ms (100.00%) | Memory: 18.1 MB (59.95%) - LeetSync

--- 733-flood-fill/flood-fill.cpp
+++ 733-flood-fill/flood-fill.cpp
@@ -1,25 +1,24 @@
 class Solution {
 public:
-    void dfs(int row,int col,int color,int originalcolor,vector<vector<int>>& image){
-        // pehle check kr lete hai ki bhai ye valid index toh hai na 
-        // original color color ke equal toh ni hai na
-        if(row<0 || row>=image.size() || col<0 || col>=image[0].size() || image[row][col]!=originalcolor || image[row][col] == color){
-            return;
-        }
-        // mark kro given color se
-        image[row][col]=color;
-        dfs(row+1,col,color,originalcolor,image);
-        dfs(row-1,col,color,originalcolor,image);
-        dfs(row,col+1,color,originalcolor,image);
-        dfs(row,col-1,color,originalcolor,image);
+    void dfs(int sr,int sc,int n,int m,vector<vector<int>>& image,int originalcolor,int color){
+        if(sr<0 || sr>=n || sc<0 || sc>=m || image[sr][sc]==color || image[sr][sc]!=originalcolor) return ;
+        // agr kahi aisa ni hai toh 
+        image[sr][sc]=color;
+        dfs(sr,sc-1,n,m,image,originalcolor,color);
+        dfs(sr,sc+1,n,m,image,originalcolor,color);
+        dfs(sr-1,sc,n,m,image,originalcolor,color);
+        dfs(sr+1,sc,n,m,image,originalcolor,color);
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
-       if(image[sr][sc]==color) return image;
-       int n=image.size();
-       int m=image[0].size();
-       int originalcolor=image[sr][sc];
-       // marking the starting index
-       dfs(sr,sc,color,originalcolor,image);
-       return image;
+        int originalcolor=image[sr][sc];
+        // if there is same color in the starting position as color.
+        if(originalcolor==color) return image;
+        // otherwise dfs start krte hai es position se.
+        int n=image.size();
+        int m=image[0].size();
+        dfs(sr,sc,n,m,image,originalcolor,color);
+        return image;
+        
+
     }
 };
