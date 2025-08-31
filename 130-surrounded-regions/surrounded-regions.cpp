49530bfe62b8081b43f4a4d30ce5b0af23d22d43
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-08-31 20:44:35 +0530

    Time: 81 ms (5.14%) | Memory: 13.8 MB (96.95%) - LeetSync

--- 130-surrounded-regions/surrounded-regions.cpp
+++ 130-surrounded-regions/surrounded-regions.cpp
@@ -1,62 +1,66 @@
 class Solution {
 public:
-    void display(vector<vector<char>>& board){
-        for(int i=0;i<board.size();i++){
-            for(int j=0;j<board[0].size();j++){
-                cout<<board[i][j]<<" ";
+    void display(vector<vector<char>>& board) {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
+                cout << board[i][j] << " ";
             }
-            cout<<endl;
+            cout << endl;
         }
     }
-    void dfs(int row,int col,vector<vector<char>>& board){
+    void dfs(int row, int col, vector<vector<char>>& board) {
         // boundary condition waalo ko chor ke
-        if(row<0 || row>board.size()-1 || col<0 || col>board[0].size()-1 || board[row][col]!='O'){
+        if (row < 0 || row > board.size() - 1 || col < 0 ||
+            col > board[0].size() - 1 || board[row][col] != 'O') {
             return;
         }
-        board[row][col]='#';
-        dfs(row+1,col,board);
-        dfs(row-1,col,board);
-        dfs(row,col+1,board);
-        dfs(row,col-1,board);
+        board[row][col] = '#';
+        dfs(row + 1, col, board);
+        dfs(row - 1, col, board);
+        dfs(row, col + 1, board);
+        dfs(row, col - 1, board);
     }
     void solve(vector<vector<char>>& board) {
-        int n=board.size();
-        int m=board[0].size();
-            // first traversing the first row
-            int firstrow=0;
-            for(int j=0;j<m;j++){
-                if(board[firstrow][j]=='O'){
-                    dfs(firstrow,j,board);
-                }
+        int n = board.size();
+        int m = board[0].size();
+        // first traversing the first row
+        int firstrow = 0;
+        for (int j = 0; j < m; j++) {
+            if (board[firstrow][j] == 'O') {
+                dfs(firstrow, j, board);
             }
-            // traversing the last row;
-            int lastrow=n-1;
-            for(int j=0;j<m;j++){
-                if(board[lastrow][j]=='O'){
-                    dfs(lastrow,j,board);
-                }
+        }
+        // traversing the last row;
+        int lastrow = n - 1;
+        for (int j = 0; j < m; j++) {
+            if (board[lastrow][j] == 'O') {
+                dfs(lastrow, j, board);
             }
-            // traversing the first column
-            int firstcol=0;
-            for(int i=0;i<n;i++){
-                if(board[i][firstcol]=='O'){
-                    dfs(i,firstcol,board);
-                }
+        }
+        // traversing the first column
+        int firstcol = 0;
+        for (int i = 0; i < n; i++) {
+            if (board[i][firstcol] == 'O') {
+                dfs(i, firstcol, board);
             }
-            // traversing the last column
-            int lastcol=m-1;
-            for(int i=0;i<n;i++){
-                if(board[i][lastcol]=='O'){
-                    dfs(i,lastcol,board);
-                }
+        }
+        // traversing the last column
+        int lastcol = m - 1;
+        for (int i = 0; i < n; i++) {
+            if (board[i][lastcol] == 'O') {
+                dfs(i, lastcol, board);
             }
-            display(board);
-            for(int i=0;i<n;i++){
-                for(int j=0;j<m;j++){
-                    if(board[i][j]=='O') board[i][j]='X';
-                    else if(board[i][j]=='#') board[i][j]='O';
-                    else continue;
-                }
+        }
+        display(board);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (board[i][j] == 'O')
+                    board[i][j] = 'X';
+                else if (board[i][j] == '#')
+                    board[i][j] = 'O';
+                else
+                    continue;
             }
+        }
     }
 };
