fd82647048611e54d9a1c6de3e3f1631aed9f26d
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-09-03 01:29:03 +0530

    Time: 0 ms (100.00%) | Memory: 9.4 MB (15.00%) - LeetSync

--- 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
+++ 1537-maximum-score-after-splitting-a-string/maximum-score-after-splitting-a-string.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int findZero(string s){
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='0') count++;
-        }
-        return count;
-    }
-    int findOne(string s){
-        int count=0;
-        for(int i=0;i<s.length();i++){
-            if(s[i]=='1') count++;
-        }
-        return count;
-    }
     int maxScore(string s) {
         int n=s.length();
+        vector<int> prefixsumZero(n,0);
+        if(s[0]=='0') prefixsumZero[0]=1;
+        for(int i=1;i<n;i++){
+            prefixsumZero[i]=prefixsumZero[i-1]+(s[i]=='0'?1:0);
+        }
+        vector<int> prefixsumOne(n,0);
+        if(s[n-1]=='1') prefixsumOne[n-1]=1;
+        for(int i=n-2;i>=0;i--){
+            prefixsumOne[i]=prefixsumOne[i+1]+(s[i]=='1'?1:0);
+        }
         int maxScore=0;
         for(int i=0;i<n-1;i++){
-            string leftstring=s.substr(0,i+1);
-            string rightstring=s.substr(i+1,n-i-1);
-            int countZeroes=findZero(leftstring);
-            int countOnes=findOne(rightstring);
-            maxScore=max(maxScore,countZeroes+countOnes);
+            int leftZerocount=prefixsumZero[i];
+            int rightOnecount=prefixsumOne[i+1];
+            maxScore=max(maxScore,leftZerocount+rightOnecount);
         }
         return maxScore;
     }
