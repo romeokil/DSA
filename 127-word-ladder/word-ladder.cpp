cbe5aff33cf1074c5bea333cc90ed2fb12bba631
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-09-05 13:14:48 +0530

    Time: 58 ms (63.53%) | Memory: 21 MB (83.65%) - LeetSync

--- 127-word-ladder/word-ladder.cpp
+++ 127-word-ladder/word-ladder.cpp
@@ -1,25 +1,27 @@
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        unordered_set<string> st(wordList.begin(),wordList.end());
-        queue<pair<string,int>> q;
-        q.push({beginWord,1});
+    int ladderLength(string beginWord, string endWord,
+                     vector<string>& wordList) {
+        unordered_set<string> st(wordList.begin(), wordList.end());
+        queue<pair<string, int>> q;
+        q.push({beginWord, 1});
         st.erase(beginWord);
-        while(!q.empty()){
-            string curword=q.front().first;
-            int steps=q.front().second;
+        while (!q.empty()) {
+            string curword = q.front().first;
+            int steps = q.front().second;
             q.pop();
-            if(curword==endWord) return steps;
-            for(int i=0;i<curword.length();i++){
-                char originalcharacter=curword[i];
-                for(char ch='a';ch<='z';ch++){
-                    curword[i]=ch;
-                    if(st.find(curword)!=st.end()){
-                        q.push({curword,steps+1});
+            if (curword == endWord)
+                return steps;
+            for (int i = 0; i < curword.length(); i++) {
+                char originalcharacter = curword[i];
+                for (char ch = 'a'; ch <= 'z'; ch++) {
+                    curword[i] = ch;
+                    if (st.find(curword) != st.end()) {
+                        q.push({curword, steps + 1});
                         st.erase(curword);
                     }
                 }
-                curword[i]=originalcharacter;
+                curword[i] = originalcharacter;
             }
         }
         return 0;
