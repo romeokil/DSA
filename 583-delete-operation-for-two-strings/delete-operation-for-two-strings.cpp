f75f1092de20f1ad3366a0cc064822d989059b4b
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-11-26 19:19:41 +0530

    Time: 15 ms (32.07%) | Memory: 16.8 MB (38.94%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.cpp
@@ -1,20 +1,27 @@
 class Solution {
 public:
-    int solve(int n,int m,string &word1,string &word2,vector<vector<int>>& dp){
-        if(n==0 || m==0) return 0;
-        if(dp[n][m]!=-1) return dp[n][m];
-        if(word1[n-1]==word2[m-1]){
-            return dp[n][m]=1+solve(n-1,m-1,word1,word2,dp);
-        }
-        return dp[n][m]=max(solve(n-1,m,word1,word2,dp),solve(n,m-1,word1,word2,dp));
-    }
     int minDistance(string word1, string word2) {
-        int n=word1.size();
-        int m=word2.size();
-        vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
-        int length_of_lcs=solve(n,m,word1,word2,dp);
-        int deletion_count=n-length_of_lcs;
-        int insertion_count=m-length_of_lcs;
-        return deletion_count+insertion_count;
+        int n=word1.length();
+        int m=word2.length();
+        vector<vector<int>> t(n+1,vector<int>(m+1,-1));
+        for(int i=0;i<=n;i++){
+            for(int j=0;j<=m;j++){
+                if(i==0 || j==0) t[i][j]=0;
+            }
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(word1[i-1]==word2[j-1]){
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else{
+                    t[i][j]=max(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        int length_of_common_subs=t[n][m];
+        int noofdeletion=n-length_of_common_subs;
+        int noofinsertion=m-length_of_common_subs;
+        return noofdeletion + noofinsertion;
     }
 };
