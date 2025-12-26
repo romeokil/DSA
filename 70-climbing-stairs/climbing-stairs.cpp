70b16a0422297e7af519502377eba82c931efd4b
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2025-12-26 10:25:17 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (98.06%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.cpp
+++ 70-climbing-stairs/climbing-stairs.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        // agr ek bhi step ni hua toh dp[0]=0;
-        // agr ek step hua toh dp[1]=1;
-        // agr 2 step hua toh dp[2]=1step+1step, 2 step =total 2 different approach =2 steps;
-        if(n==0 || n==1 || n==2) return n; 
-        dp[0]=0;
-        dp[1]=1;
-        dp[2]=2;
+        int totalSteps=0;
+        if(n==0 || n==1 || n==2 ) return n;
+        int prev=1,curr=2;
         for(int i=3;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
+            totalSteps=prev+curr;
+            prev=curr;
+            curr=totalSteps;
         }
-        return dp[n];
+        return totalSteps;
     }
 };
