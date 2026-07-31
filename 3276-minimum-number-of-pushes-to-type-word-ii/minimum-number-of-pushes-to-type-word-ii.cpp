bc97db9c3f06805af0bac457ddee2590c42f5edb
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2026-07-31 09:43:16 +0530

    Time: 8 ms (61.48%) | Memory: 25.4 MB (84.45%) - LeetSync

---
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int minimumPushes(string word) {
+        int n=word.size();
+        vector<int> freq(26,0);
+        int result=0;
+        for(int i=0;i<n;i++){
+            freq[word[i]-'a']++;
+        }
+        // descending order me sort kr diye qki jyada freq
+        // wala ko pehle rkhege tb na km baar press krna pdega.
+        sort(freq.begin(),freq.end(),greater<int>());
+        for(int i=0;i<26;i++){
+            int press= i/8+1;
+            int freqcount=freq[i];
+            result+=press*freqcount;
+        }
+        return result;
+    }
+};
