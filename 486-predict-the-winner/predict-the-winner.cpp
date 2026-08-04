301b77dc2c64fdd8f4e7049bebd194e154747ea0
Rahul Kumar <123149490+romeokil@users.noreply.github.com>
2026-08-04 23:30:14 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (22.07%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
-    int findaliceBobScoreDiff(int i,int j,vector<int>& nums){
+    int findaliceBobScoreDiff(int i,int j,vector<int>& nums,vector<vector<int>>& dp){
         if(i>j) return 0;
         if(i==j) return nums[i];
-        int take_i=nums[i]-findaliceBobScoreDiff(i+1,j,nums);
-        int take_j=nums[j]-findaliceBobScoreDiff(i,j-1,nums);
+        if(dp[i][j]!=-1) return dp[i][j];
+        int take_i=nums[i]-findaliceBobScoreDiff(i+1,j,nums,dp);
+        int take_j=nums[j]-findaliceBobScoreDiff(i,j-1,nums,dp);
 
-        return max(take_i,take_j);
+        return dp[i][j]=max(take_i,take_j);
     }
     bool predictTheWinner(vector<int>& nums) {
         int n=nums.size();
-        int aliceBobScoreDiff=findaliceBobScoreDiff(0,n-1,nums);
+        // memoize kr de rhe hai bs.
+        vector<vector<int>> dp(n+1,vector<int>(n+1,-1));
+        int aliceBobScoreDiff=findaliceBobScoreDiff(0,n-1,nums,dp);
         if(aliceBobScoreDiff>=0) return true;
         return false;
         
